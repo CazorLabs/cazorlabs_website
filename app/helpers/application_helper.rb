e8f09bacc492eb73b6d3a214779871d7feece1ba
CazorLabs <159483693+CazorLabs@users.noreply.github.com>
2025-01-17 00:19:12 +0000

    Add helper method to render icons with optional CSS classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   def application_title
     "CazorLabs"
   end
+
+  def render_icon(icon, classes: nil)
+    classes ||= ""
+    render "icons/#{icon}", classes: classes
+  end
 end
